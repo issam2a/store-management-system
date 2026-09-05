2523c505fef654cabf559bacd1c60d362c849ac7
issam <your.email@example.com>
2026-09-05 20:08:59 +0300

    Configure Django admin for all business models

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CustomerPayment, SupplierPayment
 
-# Register your models here.
+admin.site.register(CustomerPayment)
+admin.site.register(SupplierPayment)
--- apps/transactions/admin.py
+++ apps/transactions/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import TransactionCancellation
 
-# Register your models here.
+admin.site.register(TransactionCancellation)
